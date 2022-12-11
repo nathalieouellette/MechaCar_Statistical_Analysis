4b9906069d8ab4b7dbbbce2701b5468a2144d5bf
Nathalie Ouellette <nathalieaouellette@gmail.com>
2022-12-11 16:03:40 -0500

    Part 3 calculations done

--- Starter_Code/MechaCarChallenge.RScript.R
+++ Starter_Code/MechaCarChallenge.RScript.R
@@ -18,3 +18,11 @@ total_summary <- suspension_coil_table %>% summarize(Mean=mean(PSI), Median=medi
 
 ##lot summary
 lot_summary <- suspension_coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+#Part 3: T-Tests on Suspension Coils
+## Determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch
+t.test(log10(suspension_coil_table$PSI), mu=mean(log10(1500)))
+##determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch
+t.test(log10(subset(suspension_coil_table, Manufacturing_Lot == "Lot1", select = PSI)), mu=mean(log10(1500)))
+t.test(log10(subset(suspension_coil_table, Manufacturing_Lot == "Lot2", select = PSI)), mu=mean(log10(1500)))
+t.test(log10(subset(suspension_coil_table, Manufacturing_Lot == "Lot3", select = PSI)), mu=mean(log10(1500)))
